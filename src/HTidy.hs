2a09561a8039a24855be96c04599f61f5701a1db
David Baynard <davidbaynard@gmail.com>
2016-07-08 00:37:22 +0100

    Convert to use xml-conduit instead

--- src/HTidy.hs
+++ src/HTidy.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE PartialTypeSignatures, NamedWildCards #-}
 
 module HTidy
     ( filterHtml
@@ -6,11 +7,16 @@ module HTidy
 
 import BasicPrelude
 
-import Text.HTML.TagSoup
-
-import Data.String.Conv
+import Data.Conduit
+import Data.Conduit.Binary
+import System.IO
+import Text.XML.Stream.Parse
+import Text.XML.Stream.Render
 
 filterHtml :: IO ()
-filterHtml = putStr . renderTags . toStrict . parseTags =<< getContents
+filterHtml = runConduit $ sourceHandle stdin $$ parse =$= render =$= sinkHandle stdout
     where
-        toStrict = fmap . fmap $ toS
+        parse = parseBytes def
+        render = renderBytes def{rsPretty = True}
+
+
