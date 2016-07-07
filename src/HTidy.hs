9db4357f0c93e54947009a2a4f603fb2d0c65d5e
David Baynard <davidbaynard@gmail.com>
2016-07-08 00:43:23 +0100

    Use html-conduit

--- src/HTidy.hs
+++ src/HTidy.hs
@@ -10,13 +10,15 @@ import BasicPrelude
 import Data.Conduit
 import Data.Conduit.Binary
 import System.IO
+
+import Text.HTML.DOM
 import Text.XML.Stream.Parse
 import Text.XML.Stream.Render
 
 filterHtml :: IO ()
 filterHtml = runConduit $ sourceHandle stdin $$ parse =$= render =$= sinkHandle stdout
     where
-        parse = parseBytes def
+        parse = eventConduit
         render = renderBytes def{rsPretty = True}
 
 
